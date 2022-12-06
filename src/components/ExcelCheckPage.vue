c82df85be450cee64883b4e01e35f4dcafa9e87c
ranyk <1390851149@qq.com>
2022-12-06 15:20:28 +0800

    1. 修改 excel检查界面,增加文件上传组件

--- src/components/ExcelCheckPage.vue
+++ src/components/ExcelCheckPage.vue
@@ -3,6 +3,14 @@
     <el-page-header @back="returnHelloWorld" content="excel信息检查页面" style="margin-top: -2%">
     </el-page-header>
     <h1>excel 检查界面</h1>
+    <el-upload
+      class="upload-demo"
+      drag
+      action="https://jsonplaceholder.typicode.com/posts/"
+      multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    </el-upload>
   </div>
 </template>
 
